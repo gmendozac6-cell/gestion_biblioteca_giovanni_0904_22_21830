8068b76d6a1efd7d52bde0eb08ed09ca4236b954
umg <umg@HUE-LAB-1B1.labhue.local>
2026-04-25 10:19:16 -0600

    "He creado el proyecto y su estructura de paquetes"

---
+++ gestion_biblioteca_giovanni_0904_22_21830/src/main/java/com/giovanni_0904_22_21830/gestionbiblioteca/Gestion_biblioteca_giovanni_0904_22_21830.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.giovanni_0904_22_21830.gestionbiblioteca;
+
+/**
+ *
+ * @author umg
+ */
+public class Gestion_biblioteca_giovanni_0904_22_21830 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
